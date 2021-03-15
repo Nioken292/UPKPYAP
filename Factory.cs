a0eb38792857aa303fd5f5d41a2f4335b3968fd0
Рубо Артем Сергеевич <318218@college.local>
2021-03-15 10:46:22 +0300

    Second Commit

---
+++ Factory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UPLab1
+{
+    public class Factory
+    {
+        string Name { get; set; }
+        string Adress { get; set; }
+        int Year { get; set; }
+    }
+}
