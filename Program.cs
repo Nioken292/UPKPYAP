cf1fc7313b1b98a2a407a66127f4288fe9bc7af3
Рубо Артем Сергеевич <318218@college.local>
2021-03-15 11:51:02 +0300

    ProgrammCreatedCommit

--- Program.cs
+++ Program.cs
@@ -10,6 +10,31 @@ namespace UPLab1
     {
         static void Main(string[] args)
         {
+            List<Factory> FactoryList = new List<Factory>();
+            while (true)
+            {
+                Console.WriteLine("1. добавить новый");
+                Console.WriteLine("2. Вывести всё");
+                ConsoleKeyInfo PressedKey = Console.ReadKey();
+                if(PressedKey.KeyChar == '1')
+                {
+                    Console.WriteLine("Введите имя: ");
+                    string Name = Console.ReadLine();
+                    Console.WriteLine("Введите Адресс: ");
+                    string Adress = Console.ReadLine();
+                    Console.WriteLine("Введите год основания: ");
+                    int Year = Convert.ToInt32(Console.ReadLine());
+                    FactoryList.Add(new Factory(Name, Adress, Year));
+                    Console.Clear();
+                }
+                if(PressedKey.KeyChar == '2')
+                {
+                    for(int i = 0; i < FactoryList.Count; i++)
+                    {
+                        Console.WriteLine("Имя: " + FactoryList[i].Name + " Адресс: " + FactoryList[i].Adress + " Год основания: " + FactoryList[i].Year);
+                    }
+                }
+            }
         }
     }
 }
